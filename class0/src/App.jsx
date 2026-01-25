9bbf9bb9a633b7d060ace05235b0aa117fba4c38
Jelaance0o <162440781+Jelaance0o@users.noreply.github.com>
2026-01-25 11:52:37 +0530

    class0

---
+++ class0/src/App.jsx
@@ -0,0 +1,16 @@
+// const app = ()=>{
+//   return <div>
+//     <h1>Hello</h1>
+//     <h2>Hello</h2>
+//   </div>
+// }
+
+// export default 
+
+
+const App =()=>{
+  return (
+    <div>Hello Kaise ho</div>
+  )
+}
+export default App
